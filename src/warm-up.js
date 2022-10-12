3ffa77818dbeeab74242697d295ffbcee692fd13
Quynh Luu <quynhtlluu@gmail.com>
2022-10-12 13:46:12 -0700

    Add answers to 'Conditionals' section

--- src/warm-up.js
+++ src/warm-up.js
@@ -58,14 +58,22 @@ checkStock();
 // the statement 'I'm going to eat some [food]!', otherwise, log the statement 
 // 'We need to get some more [food].'
 
-
+if (stockCount > 1) {
+    console.log(`I'm going to eat some ${food}!`)
+} else {
+    console.log(`We need to get some more ${food}.`)
+}
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
 
-
+if (isFavorite === true) {
+    console.log(`My favorite food is ${food}!`)
+} else {
+    console.log(`I recently ate ${food}.`)
+}
 
 //------------------------- Arrays -------------------------//
 
