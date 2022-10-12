ab64a8472548cf7f9d196857f76d9e1396bce72b
Quynh Luu <quynhtlluu@gmail.com>
2022-10-12 15:51:41 -0700

    Add nameMenuItem, createMenuItem, and addIngredients functions

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,31 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+    this.menuItemName = menuItemName;
+    return "Delicious " + menuItemName
 }
 
+function createMenuItem(name, price, type) {
+   menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem
+}
+
+var ingredients = [];
+function addIngredients(ingredient, ingredients) {
+  ingredients.push(ingredient);
+  for (var i = 0; i < ingredients.length - 1; i++) {
+    if (ingredient === ingredients[i]) {
+      ingredients.pop();
+    }
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
